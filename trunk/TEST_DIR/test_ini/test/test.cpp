d1516763875db20d4ae90dafbcaa14b54af5f480
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-07-18 09:14:29 +0000

    整合了域名备案的配置文件读写功能

--- trunk/TEST_DIR/test_ini/test/test.cpp
+++ trunk/TEST_DIR/test_ini/test/test.cpp
@@ -2,30 +2,68 @@
 //
 
 #include "stdafx.h"
-#include "Ini.h"
+//#include "Ini.h"
+//
+//using namespace QNA;
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	TCHAR szSection[MAX_PATH] = _T("AIZHAN");
+//	TCHAR szEntry[MAX_PATH] = _T("Regex");
+//	CIni objTem(_T("D:\\config.ini"));
+//
+//	std::tstring str = objTem.GetString(szSection, szEntry);
+//	_tcscpy_s(szSection, _T("PB"));
+//	_tcscpy_s(szEntry, _T("Port"));
+//
+//	int Port = objTem.GetInt(szSection, szEntry);
+//	int Port2 = objTem.GetInt(_T("DC"), _T("Port"));
+//	bool bRet = objTem.WriteBoolean(_T("DC"), _T("Bool"), FALSE);
+//	bool bRet2 = objTem.GetBoolean(_T("PB"), _T("Bool"));
+//	bRet = objTem.GetBoolean(_T("DC"), _T("Bool"));
+//	return 0;
+//}
 
-using namespace QNA;
+#include "Config.h"
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TCHAR szSection[MAX_PATH] = _T("AIZHAN");
-	TCHAR szEntry[MAX_PATH] = _T("Regex");
-	CIni objTem(_T("D:\\config.ini"));
+	CConfig clsConfig;
+	clsConfig.Init();
 
-	std::tstring str = objTem.GetString(szSection, szEntry);
+	SERVER stuDCServer = {0};
 
-	_tcscpy_s(szSection, _T("PB"));
-	_tcscpy_s(szEntry, _T("Port"));
+	if (!clsConfig.GetServer(stuDCServer, true))
+	{
+		return 1;
+	}
 
-	int Port = objTem.GetInt(szSection, szEntry);
-	int Port2 = objTem.GetInt(_T("DC"), _T("Port"));
+	SERVER stuPBServer = {0};
 
-	bool bRet = objTem.WriteBoolean(_T("DC"), _T("Bool"), FALSE);
+	if (!clsConfig.GetServer(stuPBServer, false))
+	{
+		return 1;
+	}
 
-	bool bRet2 = objTem.GetBoolean(_T("PB"), _T("Bool"));
+	QUERYINFO stuQueryInfo1 = {0};
+	QUERYINFO stuQueryInfo2 = {0};
+	QUERYINFO stuQueryInfo3 = {0};
+	QUERYINFO stuQueryInfo4 = {0};
+	QUERYINFO stuQueryInfo5 = {0};
+	int iRet1 = clsConfig.GetWebQueryInfo(stuQueryInfo1);
+	stuQueryInfo2 = stuQueryInfo1;
+	int iRet2 = clsConfig.GetWebQueryInfo(stuQueryInfo2);
+	stuQueryInfo3 = stuQueryInfo2;
+	int iRet3 = clsConfig.GetWebQueryInfo(stuQueryInfo3);
+	stuQueryInfo4 = stuQueryInfo3;
+	int iRet4 = clsConfig.GetWebQueryInfo(stuQueryInfo4);
+	stuQueryInfo5 = stuQueryInfo4;
+	int iRet5 = clsConfig.GetWebQueryInfo(stuQueryInfo5);
+	if (!iRet5)
+	{
+		bool bRet1 = clsConfig.ResetQuery(stuQueryInfo5);
+	}
 
-	bRet = objTem.GetBoolean(_T("DC"), _T("Bool"));
 
 
 	return 0;
 }
-
