62ab7cdf15d4a0f5205671870bbf500d4eb2844d
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-07-24 09:28:26 +0000

    Socket通信类基于TCP

---
+++ trunk/TEST_DIR/test_socket/test_client/test_client.cpp
@@ -0,0 +1,24 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+#include <initsock.h>
+#include <comm_protocol.h>
+#include <Windows.h>
+CInitSock initSock;		// 初始化Winsock库
+
+////连接服务
+//int ConnectServer(LPCTSTR lpszIP, UINT uiPort, SOCKET sk)
+//{
+//
+//	return 1;
+//}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
