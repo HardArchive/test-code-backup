3ba56a3a79d076a6d4ec6749238951c957511663
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-08-14 08:48:36 +0000

    网页下载工具WTL版本

---
+++ WTL/DownLoad/DownLoad/AboutDlg.cpp
@@ -0,0 +1,24 @@
+// aboutdlg.cpp : implementation of the CAboutDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "resource.h"
+
+#include "aboutdlg.h"
+
+LRESULT CAboutDlg::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+	CenterWindow(GetParent());
+	m_HyperLink9CPP.Attach(GetDlgItem(IDC_STATIC_9CPP));
+	m_HyperLink9CPP.SetLink(_T("¾ÍC++"), _T("http://www.9cpp.com/"), TRUE); //"mailto:jun_01@163.com"
+	m_LinkIcon.Attach(GetDlgItem(IDC_STATIC_ICON));
+	//m_HyperLink9CPP.SetLink(NULL, _T("http://www.9cpp.com/"), FALSE);
+	return TRUE;
+}
+
+LRESULT CAboutDlg::OnCloseCmd(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	EndDialog(wID);
+	return 0;
+}
---
+++ WTL/DownLoad/DownLoad/DownLoad.h
@@ -0,0 +1,1 @@
+// DownLoad.h
