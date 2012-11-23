4cb1178c71a1fdbff0434bc94af6cecf7a19f981
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-11-23 10:08:17 +0000

    网络学习示例

---
+++ trunk/WTL/NetWork/Example/RealClient/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by RealClient.rc
+//
+#define IDD_REALCLIENT_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_SEND                   1001
+#define IDC_BTN_SEND                    1002
+#define IDC_BTN_CLEAR                   1003
+#define IDC_IPADDRESS1                  1005
+#define IDC_EDIT_PORT                   1006
+#define IDC_BTN_ADD                     1007
+#define IDC_BTN_SUB                     1008
+#define IDC_BUTTON3                     1009
+#define IDC_PROGRESS1                   1010
+#define IDC_EDIT_RECV                   1011
+#define IDC_BTN_CONNECT                 1012
+#define IDC_BTN_DISCONNECT              1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
