b173386480d9e7d45a3b22e4e38de3b6b258134e
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-11-27 13:11:00 +0000

    木马服务端开始编码

---
+++ trunk/WTL/NetWork/NetWorkServer/NetworkHelpers.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class CNetworkHelpers
+{
+public:
+	CNetworkHelpers(void);
+	~CNetworkHelpers(void);
+public:
+	bool CNetworkHelpersInit();      //初始化函数  设置IO模型
+
+	int SetIOModel();  //设置IO模型
+
+
+
+
+
+};
