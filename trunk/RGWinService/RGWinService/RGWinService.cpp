78d6c51f1334560ab228072d8f81e56faf1219ed
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2012-12-19 09:49:52 +0000

    服务框架搭建 未完成 需要线程池 内存池支持

--- trunk/RGWinService/RGWinService/RGWinService.cpp
+++ trunk/RGWinService/RGWinService/RGWinService.cpp
@@ -6,6 +6,17 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//SetPriorityClass 中参数，分别对应 5 种进程优先权
+	//	NORMAL_PRIORITY_CLASS    //标准
+	//	IDLE_PRIORITY_CLASS  //空闲(低)
+	//	HIGH_PRIORITY_CLASS  //高
+	//	REALTIME_PRIORITY_CLASS  //实时
+	//	BELOW_NORMAL_PRIORITY_CLASS  //低于标准
+	//	ABOVE_NORMAL_PRIORITY_CLASS  //高于标准
+	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+
+
+	Sleep(1000);
 	return 0;
 }
 
---
+++ trunk/RGWinService/common/MessageboxUtil.h
@@ -0,0 +1,38 @@
+//file MessageboxUtil.h 
+/***************************************************************************************************
+* 1、 Class      ： CMessageboxUtil
+* 2、 Version    ： 1.0
+* 3、 Description： 很简单的功能 只是让Messagebox支持无限参数
+* 4、 Author     ： RG (http://www.9cpp.com/)
+* 5、 Created    ： 2012-9-9 13:34:13
+* 6、 History    ： 
+* 7、 Remark     ： 
+****************************************************************************************************/
+#ifndef __MESSAGEBOX_UTIL_H__
+#define __MESSAGEBOX_UTIL_H__
+namespace RG
+{
+#include <stdio.h>
+#include <stdarg.h>
+#include <windows.h>
+	//无限参数的Messagebox调用
+	//参数意思与标准的一样，只是换了个位置
+	int CDECL MessageboxUtil(HWND hWnd, UINT uType, TCHAR * szCaption, TCHAR * szFormat, ...)
+	{
+		TCHAR szBuffer [1024] = {0};
+		va_list pArgList ;
+		va_start (pArgList, szFormat);
+		_vsntprintf (szBuffer, sizeof (szBuffer) / sizeof (TCHAR), 
+			szFormat, pArgList);
+		va_end (pArgList) ;
+		return MessageBox(hWnd, szBuffer, szCaption, uType);
+	}
+	
+	//正常的Messagebox调用
+	int CDECL MessageboxUtil(HWND hWnd,	LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
+	{
+		return MessageBox(hWnd, lpText, lpCaption, uType);
+	}
+}
+#endif
+
