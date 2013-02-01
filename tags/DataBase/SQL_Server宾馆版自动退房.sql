bbe0dbe094c858880b70318d562fcf55706af053
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-02-01 06:11:06 +0000

    SQL_Server宾馆版自动退房.sql 这里可以学习到 1、SQL随机数计算 2、SQL选择插入 3、SQL选择更新

--- tags/DataBase/SQL_Server宾馆版自动退房.sql
+++ tags/DataBase/SQL_Server宾馆版自动退房.sql
@@ -17,42 +17,28 @@ SELECT  sUserID ,
         sDay
 FROM    dbo.tRoom 
 
---取得插入tRoomUserInfo数据
-SELECT  sRoomID ,
-        sRoomName ,
-        dbo.tUser.sUserID ,
-        dtValidEndTime ,
-        sIDNo ,
-        sPassword ,
-        sDay ,
-        sIDCode ,
-        sComputerKey ,
-        sUserName
-FROM    dbo.tRoom ,
-        dbo.tUser
-WHERE   dbo.tUser.sUserID = dbo.tRoom.sUserID
-        AND dbo.tUser.sUserID = '6501021000998820130129174616_1'
-
+--插入tRoomUserInfo数据
 INSERT  INTO dbo.tRoomUserInfo
         SELECT  sRoomID ,
                 sRoomName ,
                 dbo.tUser.sUserID ,
-                dtValidEndTime ,
+                dtValidEndTime = GETDATE() ,
                 sIDNo ,
                 sPassword ,
                 sDay ,
                 sIDCode ,
                 sComputerKey ,
                 sUserName
-        FROM    dbo.tRoom
-                INNER JOIN dbo.tUser ON dbo.tUser.sUserID = '6501021000998820130129174616_1'
-                                        AND dbo.tUser.sUserID = dbo.tRoom.sUserID
-
+        FROM    dbo.tRoom ,
+                dbo.tUser
+        WHERE   dbo.tUser.sUserID = dbo.tRoom.sUserID
+                AND dbo.tUser.sUserID = '6501021000998820130201105252_1'
+--更新tRoomUserInfo数据
 UPDATE  dbo.tRoomUserInfo
 SET     dbo.tRoomUserInfo.sRoomID = dbo.tRoom.sRoomID ,
         dbo.tRoomUserInfo.sRoomName = dbo.tRoom.sRoomID ,
         dbo.tRoomUserInfo.sUserID = dbo.tUser.sUserID ,
-        dbo.tRoomUserInfo.dtValidEndTime = dbo.tRoom.dtValidEndTime ,
+        dbo.tRoomUserInfo.dtValidEndTime = GETDATE() ,
         dbo.tRoomUserInfo.sIDNo = dbo.tUser.sIDNo ,
         dbo.tRoomUserInfo.sPassword = dbo.tRoom.sPassword ,
         dbo.tRoomUserInfo.sDay = dbo.tRoom.sDay ,
@@ -61,8 +47,9 @@ SET     dbo.tRoomUserInfo.sRoomID = dbo.tRoom.sRoomID ,
         dbo.tRoomUserInfo.sUserName = tuser.sUserName
 FROM    dbo.tUser ,
         dbo.tRoom
-WHERE   dbo.tUser.sUserID = '6501021000998820130129174616_1'
+WHERE   dbo.tUser.sUserID = '6501021000998820130201105252_1'
         AND dbo.tUser.sUserID = dbo.tRoom.sUserID
+
 
 --取得下机数据    
 SELECT  dbo.tComputer.sComputerIP ,
@@ -103,24 +90,7 @@ SELECT  dbo.tComputer.sComputerIP ,
         dbo.tComputer.sComputerName
 FROM    dbo.tComputer
 
-
-UPDATE  dbo.tRoomUserInfo
-SET     dbo.tRoomUserInfo.sRoomID = dbo.tRoom.sRoomID ,
-        dbo.tRoomUserInfo.sRoomName = dbo.tRoom.sRoomName ,
-        dbo.tRoomUserInfo.sUserID = dbo.tUser.sUserID ,
-        dbo.tRoomUserInfo.dtValidEndTime = dbo.tRoom.dtValidEndTime ,
-        dbo.tRoomUserInfo.sIDNo = dbo.tUser.sIDNo ,
-        dbo.tRoomUserInfo.sPassword = dbo.tRoom.sPassword ,
-        dbo.tRoomUserInfo.sDay = dbo.tRoom.sDay ,
-        dbo.tRoomUserInfo.sIDCode = dbo.tUser.sIDCode ,
-        dbo.tRoomUserInfo.sComputerKey = dbo.tRoom.sComputerKey ,
-        dbo.tRoomUserInfo.sUserName = tuser.sUserName
-FROM    dbo.tUser ,
-        dbo.tRoom
-WHERE   dbo.tUser.sUserID = '6501021000998820130129174616_1'
-        AND dbo.tUser.sUserID = dbo.tRoom.sUserID
-        
-        
+     
 SELECT  dbo.tComputer.sComputerIP ,
         dbo.tComputer.sComputerMac ,
         dbo.tComputer.sComputerNo ,
@@ -186,3 +156,64 @@ FROM    dbo.tComputer
 SELECT *FROM dbo.tUser WHERE dbo.tUser.sUserID = '6501021000998820130129174616_1'	
 SELECT * FROM dbo.tComputer WHERE dbo.tComputer.sUserID = '6501021000998820130129174616_1'	
 
+
+--自动下机后清除表
+INSERT  INTO dbo.tUserInOutLog
+        ( sUserID ,
+          sUserName ,
+          bSex ,
+          sIDCode ,
+          sIDNo ,
+          dtLogTime ,
+          sComputerIP ,
+          sComputerName ,
+          sComputerNo ,
+          sComputerMac ,
+          dtOfflineTime ,
+          sAccount ,
+          bAction ,
+          iStatus ,
+          bHasSent ,
+          bHasSent1
+        )
+        SELECT  dbo.tUser.sUserID ,
+                dbo.tUser.sUserName ,
+                dbo.tUser.bSex ,
+                dbo.tUser.sIDCode ,
+                dbo.tUser.sIDNo ,
+                dbo.tUser.dtLoginTime ,
+                dbo.tComputer.sComputerIP ,
+                dbo.tComputer.sComputerName ,
+                dbo.tComputer.sComputerNo ,
+                dbo.tComputer.sComputerMac ,
+                GETDATE() ,
+                dbo.tComputer.sAccount ,
+                1 ,
+                2 ,
+                0 ,
+                0
+        FROM    dbo.tComputer ,
+                dbo.tUser
+        WHERE   dbo.tUser.sUserID = dbo.tComputer.sUserID
+                AND dbo.tUser.sUserID = '6501021000998820130130154827_1'
+        
+SELECT * FROM dbo.tUserInOutLog WHERE sUserID = '6501021000998820130130171849_1'
+DELETE dbo.tUserInOutLog WHERE sUserID = '6501021000998820130130171849_1'
+
+
+
+--产生6们随机数
+SELECT RIGHT(('000000'+cast(ceiling(rand() * 1000000) as VARCHAR(6))), 6) 
+SELECT RIGHT(('000000'+cast(floor(rand() * 1000000) as VARCHAR(6))), 6) 
+
+--随机数更新密码语句
+ DECLARE @RandomNum VARCHAR(6) 
+ SET @RandomNum = RIGHT(( '000000'
+                          + CAST(CEILING(RAND() * 1000000) AS VARCHAR(6)) ), 6)  
+ UPDATE dbo.tRoom
+ SET    dbo.tRoom.sPassword = @RandomNum
+ WHERE  dbo.tRoom.sPassword != @RandomNum
+        AND dbo.tRoom.sRoomID = '6501021000998820130131162059_1'
+        
+ SELECT dbo.tRoom.sPassword
+ FROM   dbo.tRoom
