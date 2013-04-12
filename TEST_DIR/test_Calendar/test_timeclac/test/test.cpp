6e898f0246d868ca416474292a4e31b3eeec7a06
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-04-12 03:11:01 +0000

    测试了时间获取 与DATA时间计算的ATL函数

--- TEST_DIR/test_Calendar/test_timeclac/test/test.cpp
+++ TEST_DIR/test_Calendar/test_timeclac/test/test.cpp
@@ -4,13 +4,28 @@
 #include "stdafx.h"
 #include "DateTimeN.h"
 using namespace Matr;
-int _tmain1(int argc, _TCHAR* argv[])
+//#include <windows.h>
+//#include <atlstr.h>
+//#include <stdlib.h>
+//#include <malloc.h>
+//#include <memory.h>
+//#include <tchar.h>
+//#include <stdio.h>
+#include <ATLComTime.h>
+#define DateTimeToString(dt) COleDateTime(dt).Format("%Y-%m-%d %H:%M:%S")
+#define DateToString(dt) COleDateTime(dt).Format("%Y-%m-%d")
+#define CURRENTDATE COleDateTime::GetCurrentTime().Format("%Y%m%d")
+#define CURRENTTIME COleDateTime::GetCurrentTime().Format("%H%M")
+#define GETCURRENTTIME COleDateTime::GetCurrentTime().Format("%Y-%m-%d %H:%M:%S")
+int _tmain(int argc, _TCHAR* argv[])
 {
-
+	DATE dt = 41375.729167; 
+	CString pTime = DateTimeToString(dt);
 	DateTimeN obj1(2012, 04, 27, 15, 05);
 	DateTimeN obj2(2012, 04, 26, 13, 05); //201203311822
 	DateTimeN obj3 = obj1 -obj2;
 	int iDay = obj3.hour();
+
 	return 0;
 }
 
