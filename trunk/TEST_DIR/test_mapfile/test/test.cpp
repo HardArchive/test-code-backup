35b6c2c69124a8a84204317bc2f5c76c0b143767
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-04-27 11:26:11 +0000

    mapfile调适功能测试

---
+++ trunk/TEST_DIR/test_mapfile/test/test.cpp
@@ -0,0 +1,26 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+//#include "stdafx.h"
+//
+//
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
+#include "stdafx.h"
+
+
+void errorFun(int * p)
+{
+	*p=1;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	int * p=NULL;
+	errorFun(p);
+	return 0;
+}
