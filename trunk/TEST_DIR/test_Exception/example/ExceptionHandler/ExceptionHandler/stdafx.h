198fdd49e1e95af2dec3b83c5bdc03f0634ea45c
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-05-15 11:34:30 +0000

    异常处理的例子和文档

---
+++ trunk/TEST_DIR/test_Exception/example/ExceptionHandler/ExceptionHandler/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <new.h>
+#include <signal.h>
+#include <exception>
+#include <sys/stat.h>
+#include <psapi.h>
+#include <rtcapi.h>
+#include <Shellapi.h>
+#include <dbghelp.h>
+
+
+// TODO: reference additional headers your program requires here
