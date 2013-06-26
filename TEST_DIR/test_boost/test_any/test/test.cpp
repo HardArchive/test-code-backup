3bc2109cebbcdbe54bc8bc594f1ede11a9b3d1a1
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-06-26 06:46:48 +0000

    boost any 示例

--- TEST_DIR/test_boost/test_any/test/test.cpp
+++ TEST_DIR/test_boost/test_any/test/test.cpp
@@ -11,6 +11,11 @@
 //
 //
 
+//任意的东西！
+//
+//考虑三个类，A, B, 和 C, 它们没有共同的基类，而我们想把它们存入一个 std::vector.
+// 如果它们没有共同基类，看起来我们不得不把它们当成 void* 来保存，对吗？唔，not any more (相关语，没有更多的了)，
+// 因为类型 any 没有改变对所存值的类型的依赖。以下代码示范了如何解决这个问题。
 
 #include <iostream>
 #include <string>
@@ -46,6 +51,41 @@ public:
     }
 };
 
+void print_any(boost::any &a)
+{
+	//any_cast 让你访问any中存放的值。参数为需要取回值的 any 对象。
+	// 如果类型 ValueType 与所存值不符，any 抛出一个 bad_any_cast 异常。
+	// 请注意，这个语法有点象 dynamic_cast.
+    if (A *pA = boost::any_cast<A>(&a))
+    {
+        pA->some_function();
+    }
+    else if (B *pB = boost::any_cast<B>(&a))
+    {
+        pB->some_function();
+    }
+    else if (C *pC = boost::any_cast<C>(&a))
+    {
+        pC->some_function();
+    }
+	else 
+	{
+		//异常
+		// bad_any_cast
+		// 当试图将一个any对象转换为该对象所存类型以外的其它类型，将抛出该异常。
+		// bad_any_cast 派生自 std::bad_cast. 注意，使用指针参数调用 any_cast 时，将不抛出异常(类似于对指针使用 dynamic_cast 时返回空指针一样)，
+		// 反之对引用类型使用 dynamic_cast 则会在失败时抛出异常。
+		try
+		{
+			std::cout << boost::any_cast<std::string>(a) << '\r\n';
+		}
+		catch(boost::bad_any_cast&) 
+		{
+			std::cout << "Oops!\r\n";
+		}
+	}
+}
+
 int main()
 {
     std::cout << "Example of using any.\n\n";
@@ -57,13 +97,11 @@ int main()
     store_anything.push_back(C());
 
     // 我们再来，再加一些别的东西
-    store_anything.push_back(std::string("This is fantastic! "));
+    store_anything.push_back(std::string("This is fantastic!\r\n"));
     store_anything.push_back(3);
     store_anything.push_back(std::make_pair(true, 7.92));
 
-    void print_any(boost::any & a);
-    // 稍后定义；打印a中的值
-
+	//算法类的遍历方法
     std::for_each(
         store_anything.begin(),
         store_anything.end(),
