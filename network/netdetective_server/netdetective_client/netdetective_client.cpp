5fc8d5b44edf29f49ee7bf44e988b969cbcc8045
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-07-09 09:56:07 +0000

    添加client 检测当前目录功能

--- network/netdetective_server/netdetective_client/netdetective_client.cpp
+++ network/netdetective_server/netdetective_client/netdetective_client.cpp
@@ -6,6 +6,9 @@
 #include "../global/ClientHelper.h"
 
 #include "../global/ShareMemory.h"
+#include "../third_party/socket_module/FuncHelper.h"
+
+typedef TCHAR* PTCHAR;
 
 int GetXMLContecnt(const TCHAR* ptXMLPath, PBYTE pOutBuf, int iMaxBufLen)
 {
@@ -38,16 +41,36 @@ void InitHead(PDATAHEAD pstuInHead)
 	memcpy(pstuInHead->szHeadFlag, PACKAGE_MARK, 4);
 }
 
-const TCHAR g_tszXMLPath[4][MAX_PATH] ={
-	_T("F:\\Projects\\test-code-backup\\trunk\\network\\netdetective_server\\bin\\ab1.xml"),
-	_T("F:\\Projects\\test-code-backup\\trunk\\network\\netdetective_server\\bin\\ab2.xml"),
-	_T("F:\\Projects\\test-code-backup\\trunk\\network\\netdetective_server\\bin\\ab3.xml"),
-	_T("F:\\Projects\\test-code-backup\\trunk\\network\\netdetective_server\\bin\\ab4.xml"),
-};
+const TCHAR g_tszXMLPath[4][MAX_PATH] ={_T("ab1.xml"),	_T("ab2.xml"),	_T("ab3.xml"),	_T("ab4.xml")};
+
+////获取当前程序所在目录 成功返回true，失败返回false
+//bool GetExePath(PTCHAR ptInPath)
+//{
+//	PTCHAR ptTem = NULL;
+//	TCHAR tszTemp[MAX_PATH] = {0};
+//	//获取当前目录  //这里是获取当前进程文件的完整路径 
+//	if (!GetModuleFileName(NULL, tszTemp, MAX_PATH) && ptInPath)
+//		return false; 
+//
+//	ptTem = _tcsrchr(tszTemp, _T('\\'));
+//	memcpy(ptInPath, tszTemp, (_tcslen(tszTemp)-_tcslen(ptTem))*sizeof(TCHAR));
+//	return true;
+//}
+
+PTCHAR GetXmlPath(PTCHAR ptInPath, int iType)
+{
+	if (GetExePath(ptInPath))
+	{
+		strcat(ptInPath, "\\");
+		strcat(ptInPath, g_tszXMLPath[iType]);
+	}
+	return ptInPath;
+}
 
 void SendUserStatusInfo(int iSendType)
 {
 	int iSendLen = 0;
+	TCHAR tszTem[MAX_PATH] = {0};
 	BYTE szbySendBuf[2048] = {0};
 	static DWORD dwSerialNo = 0;
 	CClientHelper clsClientHelper;
@@ -55,7 +78,7 @@ void SendUserStatusInfo(int iSendType)
 	PDATAHEAD pstuHead = (PDATAHEAD)szbySendBuf;
 	InitHead(pstuHead);
 	pstuHead->dwSerialNo = dwSerialNo++;
-	pstuHead->dwPacketLen = GetXMLContecnt(g_tszXMLPath[iSendType-1], szbySendBuf+sizeof(DATAHEAD), 2048-sizeof(DATAHEAD));
+	pstuHead->dwPacketLen = GetXMLContecnt(GetXmlPath(tszTem, iSendType-1), szbySendBuf+sizeof(DATAHEAD), 2048-sizeof(DATAHEAD));
 	pstuHead->dwPacketLen += sizeof(DATAHEAD);
 	iSendLen = pstuHead->dwPacketLen;
 	clsClientHelper.Send(szbySendBuf, iSendLen);
