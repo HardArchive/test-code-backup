4e27aef445cf01fa448a58e2ddb36758e55286df
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-11-19 05:41:17 +0000

    WTL拖拽示例 //让窗口支持拖放(文件，文本，URL...) //http://pyhcx.blog.51cto.com/713166/192454

---
+++ WTL/WTLDROPFILES/FRAMESAMPLE/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__08DFD39E_85A8_46DC_B5F6_0270215CDAE5__INCLUDED_)
+#define AFX_STDAFX_H__08DFD39E_85A8_46DC_B5F6_0270215CDAE5__INCLUDED_
+
+// Change these values to use different versions
+#define WINVER		0x0400
+//#define _WIN32_WINNT	0x0400
+#define _WIN32_IE	0x0400
+#define _RICHEDIT_VER	0x0100
+
+#include <atlbase.h>
+#include <atlapp.h>
+
+extern CAppModule _Module;
+
+#include <atlwin.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__08DFD39E_85A8_46DC_B5F6_0270215CDAE5__INCLUDED_)
---
+++ WTL/WTLDROPFILES/WTLDROPFILES/WTLDROPFILES.cpp
@@ -0,0 +1,45 @@
+// WTLDROPFILES.cpp : main source file for WTLDROPFILES.exe
+//
+
+#include "stdafx.h"
+
+#include <atlframe.h>
+#include <atlctrls.h>
+#include <atldlgs.h>
+#include <atlctrlw.h>
+
+#include "resource.h"
+
+#include "maindlg.h"
+
+CAppModule _Module;
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
+{
+	HRESULT hRes = ::CoInitialize(NULL);
+// If you are running on NT 4.0 or higher you can use the following call instead to 
+// make the EXE free threaded. This means that calls come in on a random RPC thread.
+//	HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+	::DefWindowProc(NULL, 0, 0, 0L);
+
+	AtlInitCommonControls(ICC_COOL_CLASSES | ICC_BAR_CLASSES);	// add flags to support other controls
+
+	hRes = _Module.Init(NULL, hInstance);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	int nRet = 0;
+	// BLOCK: Run application
+	{
+		CMainDlg dlgMain;
+		nRet = dlgMain.DoModal();
+	}
+
+	_Module.Term();
+	::CoUninitialize();
+
+	return nRet;
+}
