423a864e45c1d7ddc70db70bbf6aa472fa03e714
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2013-12-01 10:18:00 +0000

    hp-socket-3.0.2 目前已将vs2010工程转化为2008工工程
    需要将其改造成一个通用通信组件

---
+++ trunk/WTL/NetWork/hp-socket/hp-socket-3.0.2/vc2008/Common/Src/socket/SocketHelper.cpp
@@ -0,0 +1,446 @@
+/*
+ * Copyright Bruce Liang (ldcsaa@gmail.com)
+ *
+ * Version	: 3.0.1
+ * Author	: Bruce Liang
+ * Website	: http://www.jessma.org
+ * Porject	: https://code.google.com/p/ldcsaa
+ * Bolg		: http://www.cnblogs.com/ldcsaa
+ * WeiBo	: http://weibo.com/u/1402935851
+ * QQ Group	: 75375912
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include "stdafx.h"
+#include "SocketHelper.h"
+#include <mstcpip.h>
+
+#ifndef _WIN32_WCE
+	#pragma comment(lib, "ws2_32")
+#else
+	#pragma comment(lib, "ws2")
+#endif
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+BOOL IsIPAddress(LPCTSTR lpszAddress)
+{
+	if(!lpszAddress)
+		return FALSE;
+
+	int len = (int)lstrlen(lpszAddress) + 1;
+	
+	if(len > 15)
+		return false;
+
+	int iDotIndex = 0;
+	int iDotCount = 0;
+
+	for(int i = 0; i < len; ++i)
+	{
+		TCHAR c = lpszAddress[i];
+		if((c < '0' || c > '9') && c != '.' && c != '\0')
+			return FALSE;
+		else if(c == '.' || c == '\0')
+		{
+			int iSubLen = i - iDotIndex;
+
+			if(iSubLen < 1 || iSubLen > 3)
+				return FALSE;
+
+			TCHAR szSub[4] = {0};
+			_tcsncpy(szSub, lpszAddress + iDotIndex, iSubLen);
+
+			int iSub = _ttoi(szSub);
+
+			if(iSub < 0 || iSub > 255)
+				return FALSE;
+
+			if(c == '.')
+			{
+				iDotIndex = i + 1;
+				++iDotCount;
+			}
+		}
+	}
+
+	return (iDotCount == 3 || iDotCount == 5);
+}
+
+BOOL GetIPAddress(LPCTSTR lpszHost, CString& strIP)
+{
+	BOOL isOK = TRUE;
+
+	if(IsIPAddress(lpszHost))
+		strIP = lpszHost;
+	else
+	{
+		hostent* host = ::gethostbyname(CT2A(lpszHost));
+
+		if(!host)
+			return FALSE;
+
+		strIP = inet_ntoa(*(struct in_addr*)(*host->h_addr_list)); 
+	}
+
+	return TRUE;
+}
+
+BOOL sockaddr_IN_2_A(const SOCKADDR_IN& addr, ADDRESS_FAMILY& usFamily, CString& strAddress, USHORT& usPort)
+{
+	usFamily	= addr.sin_family;
+	usPort		= ntohs(addr.sin_port);
+	strAddress	= CA2T(inet_ntoa(addr.sin_addr));
+
+	return !strAddress.IsEmpty();
+}
+
+BOOL sockaddr_A_2_IN(ADDRESS_FAMILY usFamily, LPCTSTR pszAddress, USHORT usPort, SOCKADDR_IN& addr)
+{
+	addr.sin_family			= usFamily;
+	addr.sin_port			= htons(usPort);
+	addr.sin_addr.s_addr	= inet_addr(CT2A(pszAddress));
+
+	return addr.sin_addr.s_addr != INADDR_NONE;
+}
+
+BOOL GetSocketAddress(SOCKET socket, CString& strAddress, USHORT& usPort, BOOL bLocal)
+{
+	sockaddr addr;
+
+	int addr_len = sizeof(addr);
+	int result	 = bLocal ? getsockname(socket, &addr, &addr_len) : getpeername(socket, &addr, &addr_len);
+
+	if(result == NO_ERROR)
+	{
+		ADDRESS_FAMILY usFamily;
+		return sockaddr_IN_2_A((sockaddr_in&)addr, usFamily, strAddress, usPort);
+	}
+
+	return FALSE;
+}
+
+BOOL GetSocketLocalAddress(SOCKET socket, CString& strAddress, USHORT& usPort)
+{
+	return GetSocketAddress(socket, strAddress, usPort, TRUE);
+}
+
+BOOL GetSocketRemoteAddress(SOCKET socket, CString& strAddress, USHORT& usPort)
+{
+	return GetSocketAddress(socket, strAddress, usPort, FALSE);
+}
+
+PVOID GetExtensionFuncPtr(SOCKET sock, GUID guid)
+{
+	DWORD dwBytes;
+	PVOID pfn = nullptr;
+
+	::WSAIoctl	(
+					sock,
+					SIO_GET_EXTENSION_FUNCTION_POINTER,
+					&guid,
+					sizeof(guid),
+					&pfn,
+					sizeof(pfn),
+					&dwBytes,
+					nullptr,
+					nullptr
+				);
+
+	return pfn;
+}
+
+LPFN_ACCEPTEX Get_AcceptEx_FuncPtr(SOCKET sock)
+{
+	GUID guid = WSAID_ACCEPTEX;
+	return (LPFN_ACCEPTEX)GetExtensionFuncPtr(sock, guid);
+}
+
+LPFN_GETACCEPTEXSOCKADDRS Get_GetAcceptExSockaddrs_FuncPtr(SOCKET sock)
+{
+	GUID guid = WSAID_GETACCEPTEXSOCKADDRS;
+	return (LPFN_GETACCEPTEXSOCKADDRS)GetExtensionFuncPtr(sock, guid);
+}
+
+LPFN_CONNECTEX Get_ConnectEx_FuncPtr(SOCKET sock)
+{
+	GUID guid = WSAID_CONNECTEX;
+	return (LPFN_CONNECTEX)GetExtensionFuncPtr(sock, guid);
+}
+
+LPFN_TRANSMITFILE Get_TransmitFile_FuncPtr(SOCKET sock)
+{
+	GUID guid = WSAID_TRANSMITFILE;
+	return (LPFN_TRANSMITFILE)GetExtensionFuncPtr(sock, guid);
+}
+
+LPFN_DISCONNECTEX Get_DisconnectEx_FuncPtr	(SOCKET sock)
+{
+	GUID guid = WSAID_DISCONNECTEX;
+	return (LPFN_DISCONNECTEX)GetExtensionFuncPtr(sock, guid);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int SSO_UpdateAcceptContext(SOCKET soClient, SOCKET soBind)
+{
+	return setsockopt(soClient, SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT, (CHAR*)&soBind, sizeof(SOCKET));
+}
+
+int SSO_UpdateConnectContext(SOCKET soClient, int iOption)
+{
+	return setsockopt(soClient, SOL_SOCKET, SO_UPDATE_CONNECT_CONTEXT, (CHAR*)&iOption, sizeof(int));
+}
+
+int SSO_NoDelay(SOCKET sock, BOOL bNoDelay)
+{
+	return setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (CHAR*)&bNoDelay, sizeof(BOOL));
+}
+
+int SSO_DontLinger(SOCKET sock, BOOL bDont)
+{
+	return setsockopt(sock, SOL_SOCKET, SO_DONTLINGER, (CHAR*)&bDont, sizeof(BOOL));
+}
+
+int SSO_Linger(SOCKET sock, USHORT l_onoff, USHORT l_linger)
+{
+	linger ln = {l_onoff, l_linger};
+	return setsockopt(sock, SOL_SOCKET, SO_LINGER, (CHAR*)&ln, sizeof(linger));
+}
+
+int SSO_KeepAlive(SOCKET sock, BOOL bKeepAlive)
+{
+	return setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (CHAR*)&bKeepAlive, sizeof(BOOL));
+}
+
+int SSO_KeepAliveVals(SOCKET sock, u_long onoff, u_long time, u_long interval)
+{
+	int result		 = NO_ERROR;
+	tcp_keepalive in = {onoff, time, interval};
+	DWORD dwBytes;
+
+	if(::WSAIoctl	(
+						sock, 
+						SIO_KEEPALIVE_VALS, 
+						(LPVOID)&in, 
+						sizeof(in), 
+						nullptr, 
+						0, 
+						&dwBytes, 
+						nullptr, 
+						nullptr
+					) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSAEWOULDBLOCK)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+int SSO_RecvBuffSize(SOCKET sock, int size)
+{
+	return setsockopt(sock, SOL_SOCKET, SO_RCVBUF, (CHAR*)&size, sizeof(int));
+}
+
+int SSO_SendBuffSize(SOCKET sock, int size)
+{
+	return setsockopt(sock, SOL_SOCKET, SO_SNDBUF, (CHAR*)&size, sizeof(int));
+}
+
+int SSO_ReuseAddress(SOCKET sock, BOOL bReuse)
+{
+	return setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (CHAR*)&bReuse, sizeof(BOOL));
+}
+
+int SSO_UDP_ConnReset(SOCKET sock, BOOL bNewBehavior)
+{
+	int result = NO_ERROR;
+	DWORD dwBytes;
+
+	if(::WSAIoctl	(
+						sock, 
+						SIO_UDP_CONNRESET, 
+						(LPVOID)&bNewBehavior, 
+						sizeof(bNewBehavior), 
+						nullptr, 
+						0, 
+						&dwBytes, 
+						nullptr, 
+						nullptr
+					) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSAEWOULDBLOCK)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CONNID GenerateConnectionID(volatile CONNID& dwSeed)
+{
+	CONNID dwConnID	= ::InterlockedIncrement((volatile LONG *)(&dwSeed));
+	
+	if(dwConnID == 0)
+		dwConnID = ::InterlockedIncrement((volatile LONG *)(&dwSeed));
+
+	return dwConnID;
+}
+
+int ManualCloseSocket(SOCKET sock, int iShutdownFlag, BOOL bGraceful, BOOL bReuseAddress)
+{
+	if(!bGraceful)
+		SSO_Linger(sock, 1, 0);
+
+	if(bReuseAddress)
+		SSO_ReuseAddress(sock, bReuseAddress);
+
+	if(iShutdownFlag != 0xFF)
+		shutdown(sock, iShutdownFlag);
+
+	return closesocket(sock);
+}
+
+int PostAccept(LPFN_ACCEPTEX pfnAcceptEx, SOCKET soListen, SOCKET soClient, TBufferObj* pBufferObj)
+{
+	int result				= NO_ERROR;
+	pBufferObj->client		= soClient;
+	pBufferObj->operation	= SO_ACCEPT;
+
+	if(!pfnAcceptEx	(
+						soListen,
+						pBufferObj->client,
+						pBufferObj->buff.buf,
+						0,
+						sizeof(SOCKADDR_IN) + 16,
+						sizeof(SOCKADDR_IN) + 16,
+						nullptr,
+						&pBufferObj->ov
+					)
+	)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSA_IO_PENDING)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+int PostSend(TSocketObj* pSocketObj, TBufferObj* pBufferObj)
+{
+	int result				= NO_ERROR;
+	pBufferObj->operation	= SO_SEND;
+	DWORD dwBytes;
+
+	if(::WSASend(
+					pSocketObj->socket,
+					&pBufferObj->buff,
+					1,
+					&dwBytes,
+					0,
+					&pBufferObj->ov,
+					nullptr
+				) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSA_IO_PENDING)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+int PostReceive(TSocketObj* pSocketObj, TBufferObj* pBufferObj)
+{
+	int result				= NO_ERROR;
+	pBufferObj->operation	= SO_RECEIVE;
+	DWORD dwBytes, dwFlag	= 0; 
+
+	if(::WSARecv(
+					pSocketObj->socket,
+					&pBufferObj->buff,
+					1,
+					&dwBytes,
+					&dwFlag,
+					&pBufferObj->ov,
+					nullptr
+				) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSA_IO_PENDING)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+int PostSendTo(SOCKET sock, TUdpBufferObj* pBufferObj)
+{
+	int result				= NO_ERROR;
+	pBufferObj->operation	= SO_SEND;
+	DWORD dwBytes;
+
+	if(::WSASendTo	(
+						sock,
+						&pBufferObj->buff,
+						1,
+						&dwBytes,
+						0,
+						(sockaddr*)&pBufferObj->clientAddr,
+						pBufferObj->addrLen,
+						&pBufferObj->ov,
+						nullptr
+					) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSA_IO_PENDING)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
+
+int PostReceiveFrom(SOCKET sock, TUdpBufferObj* pBufferObj)
+{
+	int result				= NO_ERROR;
+	pBufferObj->addrLen		= sizeof(SOCKADDR_IN);
+	pBufferObj->operation	= SO_RECEIVE;
+	DWORD dwBytes, dwFlag	= 0;
+
+	::ZeroMemory(&pBufferObj->clientAddr, pBufferObj->addrLen);
+
+	if(::WSARecvFrom(
+						sock,
+						&pBufferObj->buff,
+						1,
+						&dwBytes,
+						&dwFlag,
+						(sockaddr*)&pBufferObj->clientAddr,
+						&pBufferObj->addrLen,
+						&pBufferObj->ov,
+						nullptr
+					) == SOCKET_ERROR)
+	{
+		result = ::WSAGetLastError();
+		if(result == WSA_IO_PENDING)
+			result = NO_ERROR;
+	}
+
+	return result;
+}
---
+++ trunk/WTL/NetWork/hp-socket/hp-socket-3.0.2/vc2008/Common/Src/socket/UdpClient.h
@@ -0,0 +1,184 @@
+/*
+ * Copyright Bruce Liang (ldcsaa@gmail.com)
+ *
+ * Version	: 3.0.1
+ * Author	: Bruce Liang
+ * Website	: http://www.jessma.org
+ * Porject	: https://code.google.com/p/ldcsaa
+ * Bolg		: http://www.cnblogs.com/ldcsaa
+ * WeiBo	: http://weibo.com/u/1402935851
+ * QQ Group	: 75375912
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#pragma once
+
+#include "SocketHelper.h"
+#include "../Event.h"
+#include "../bufferptr.h"
+#include "../CriticalSection.h"
+
+class CUdpClient : public IUdpClient
+{
+public:
+	virtual BOOL Start	(LPCTSTR pszRemoteAddress, USHORT usPortt, BOOL bAsyncConnect = FALSE);
+	virtual BOOL Stop	();
+	virtual BOOL Send	(CONNID dwConnID, const BYTE* pBuffer, int iLength);
+	virtual BOOL			HasStarted		()	{return m_enState == SS_STARTED || m_enState == SS_STARTING;}
+	virtual EnServiceState	GetState		()	{return m_enState;}
+	virtual CONNID			GetConnectionID	()	{return m_dwConnID;};
+	virtual EnClientError	GetLastError	()	{return m_enLastError;}
+	virtual BOOL			GetLocalAddress	(CString& strAddress, USHORT& usPort);
+	virtual LPCTSTR			GetLastErrorDesc();
+
+public:
+	virtual void SetMaxDatagramSize	(DWORD dwMaxDatagramSize)	{m_dwMaxDatagramSize = dwMaxDatagramSize;}
+	virtual void SetDetectAttempts	(DWORD dwDetectAttempts)	{m_dwDetectAttempts	 = dwDetectAttempts;}
+	virtual void SetDetectInterval	(DWORD dwDetectInterval)	{m_dwDetectInterval	 = dwDetectInterval;}
+
+	virtual DWORD GetMaxDatagramSize()							{return m_dwMaxDatagramSize;}
+	virtual DWORD GetDetectAttempts	()							{return m_dwDetectAttempts;}
+	virtual DWORD GetDetectInterval	()							{return m_dwDetectInterval;}
+
+protected:
+	virtual ISocketListener::EnHandleResult FirePrepareConnect(CONNID dwConnID, SOCKET socket)
+		{return m_psoListener->OnPrepareConnect(dwConnID, socket);}
+	virtual ISocketListener::EnHandleResult FireConnect(CONNID dwConnID)
+		{return m_psoListener->OnConnect(dwConnID);}
+	virtual ISocketListener::EnHandleResult FireSend(CONNID dwConnID, const BYTE* pData, int iLength)
+		{return m_psoListener->OnSend(dwConnID, pData, iLength);}
+	virtual ISocketListener::EnHandleResult FireReceive(CONNID dwConnID, const BYTE* pData, int iLength)
+		{return m_psoListener->OnReceive(dwConnID, pData, iLength);}
+	virtual ISocketListener::EnHandleResult FireReceive(CONNID dwConnID, int iLength)
+		{return m_psoListener->OnReceive(dwConnID, iLength);}
+	virtual ISocketListener::EnHandleResult FireClose(CONNID dwConnID)
+		{return m_psoListener->OnClose(dwConnID);}
+	virtual ISocketListener::EnHandleResult FireError(CONNID dwConnID, EnSocketOperation enOperation, int iErrorCode)
+		{return m_psoListener->OnError(dwConnID, enOperation, iErrorCode);}
+
+private:
+	BOOL CheckParams();
+	BOOL CheckStarting();
+	BOOL CheckStoping();
+	BOOL CreateClientSocket();
+	BOOL ConnectToServer(LPCTSTR pszRemoteAddress, USHORT usPort);
+	BOOL CreateWorkerThread();
+	BOOL CreateDetectorThread();
+	BOOL ProcessNetworkEvent();
+	BOOL ReadData();
+	BOOL SendData();
+	CBufferPtr* GetSendBuffer();
+	void WaitForWorkerThreadEnd(DWORD dwCurrentThreadID);
+	void WaitForDetectorThreadEnd(DWORD dwCurrentThreadID);
+
+	BOOL HandleError();
+	BOOL HandleRead(WSANETWORKEVENTS& events);
+	BOOL HandleWrite(WSANETWORKEVENTS& events);
+	BOOL HandleConnect(WSANETWORKEVENTS& events);
+	BOOL HandleClosse(WSANETWORKEVENTS& events);
+
+	int DetectConnection	();
+	BOOL NeedDetectorThread	() {return m_dwDetectAttempts > 0 && m_dwDetectInterval > 0;}
+
+	void SetLastError		(EnClientError code, LPCTSTR func, int ec);
+
+	static 
+#ifndef _WIN32_WCE
+	UINT
+#else
+	DWORD
+#endif
+	 WINAPI WorkerThreadProc(LPVOID pv);
+
+	static 
+#ifndef _WIN32_WCE
+	UINT
+#else
+	DWORD
+#endif
+	 WINAPI DetecotrThreadProc(LPVOID pv);
+
+private:
+	static const DWORD DEFAULT_MAX_DATAGRAM_SIZE	= 1472;
+	static const DWORD DEFAULT_DETECT_ATTEMPTS		= 3;
+	static const DWORD DEFAULT_DETECT_INTERVAL		= 10;
+
+	static volatile CONNID	sm_dwConnID;
+
+public:
+	CUdpClient(IUdpClientListener* psoListener)
+	: m_psoListener(psoListener)
+	, m_soClient			(INVALID_SOCKET)
+	, m_evSocket			(nullptr)
+	, m_dwConnID			(0)
+	, m_hWorker				(nullptr)
+	, m_dwWorkerID			(0)
+	, m_hDetector			(nullptr)
+	, m_dwDetectorID		(0)
+	, m_bAsyncConnect		(FALSE)
+	, m_enState				(SS_STOPED)
+	, m_enLastError			(CE_OK)
+	, m_dwDetectFails		(0)
+	, m_dwMaxDatagramSize	(DEFAULT_MAX_DATAGRAM_SIZE)
+	, m_dwDetectAttempts	(DEFAULT_DETECT_ATTEMPTS)
+	, m_dwDetectInterval	(DEFAULT_DETECT_INTERVAL)
+	{
+		ASSERT(m_psoListener);
+	}
+
+	virtual ~CUdpClient()	{if(HasStarted()) Stop();}
+
+private:
+	CInitSocket		m_wsSocket;
+	SOCKET			m_soClient;
+	HANDLE			m_evSocket;
+	CONNID			m_dwConnID;
+
+	CEvt			m_evWorker;
+	HANDLE			m_hWorker;
+
+#ifndef _WIN32_WCE
+	UINT
+#else
+	DWORD
+#endif
+					m_dwWorkerID;
+
+	CEvt			m_evDetector;
+	HANDLE			m_hDetector;
+
+#ifndef _WIN32_WCE
+	UINT
+#else
+	DWORD
+#endif
+					m_dwDetectorID;
+
+	BOOL			m_bAsyncConnect;
+
+	CEvt				m_evBuffer;
+	CCriSec				m_scBuffer;
+	CBufferPtr			m_rcBuffer;
+	CAutoBufferPtrList	m_sndBuffers;
+
+	volatile EnServiceState	m_enState;
+	volatile EnClientError	m_enLastError;
+	volatile DWORD			m_dwDetectFails;
+
+private:
+	IUdpClientListener*	m_psoListener;
+	DWORD				m_dwMaxDatagramSize;
+	DWORD				m_dwDetectAttempts;
+	DWORD				m_dwDetectInterval;
+};
