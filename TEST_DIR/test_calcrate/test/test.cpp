04d5eb48f36a3b80f9f15652bd8167f5c4527f22
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2014-01-24 10:40:44 +0000

    修正了一些bug 增加 算息从元开始  得从分四舍五入 还有问题 不能从月算定存  …… 这个问题需要解决

--- TEST_DIR/test_calcrate/test/test.cpp
+++ TEST_DIR/test_calcrate/test/test.cpp
@@ -10,11 +10,12 @@
 //邮政银行	0.35 	2.85 	3.05 	3.25 	3.75 	4.25 	4.75 
 //浦发银行	0.39 	2.86 	3.08 	3.30 	3.75 	4.25 	4.75 
 
-BANKREGULARRATES g_szBankRegularRates[5] = {
+BANKREGULARRATES g_szBankRegularRates[6] = {
 	{_T("央行基准"), {{{0,1},0.35},{{1,3},2.60},{{1,6},2.80},{{0,1},3.00},{{0,2},3.75},{{0,3},4.25},{{0,5},4.75}}},
 	{_T("招商银行"), {{{0,1},0.39},{{1,3},2.86},{{1,6},3.08},{{0,1},3.30},{{0,2},3.75},{{0,3},4.25},{{0,5},4.75}}},
 	{_T("邮政银行"), {{{0,1},0.35},{{1,3},2.85},{{1,6},3.05},{{0,1},3.25},{{0,2},3.75},{{0,3},4.25},{{0,5},4.75}}},
-	{_T("浦发银行"), {{{0,1},0.39},{{1,3},2.86},{{1,6},3.08},{{0,1},3.30},{{0,2},3.75},{{0,3},4.25},{{0,5},4.75}}},
+	{_T("浦发银行"), {{{0,1},0.39},{{1,3},2.86},{{1,6},3.08},{{0,1},3.30},{{0,2},3.75},{{0,3},4.25},{{0,5},4.75}}},	
+	{_T("中信银行"), {{{0,1},0.39},{{1,3},2.86},{{1,6},3.08},{{0,1},3.30},{{0,2},4.125},{{0,3},4.675},{{0,5},5.225}}},
 	{_T("货币基金"), {{{0,1},5.00},{{0,1},5.00},{{0,1},5.00},{{0,1},5.00},{{0,1},5.00},{{0,1},5.00},{{0,1},5.00}}},
 };
 //10K 存 3年期 存18年 20544.676316626468
@@ -26,7 +27,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	PREGULARRATES pstuRegularates4 = &g_szBankRegularRates[1].stuRegularRates[4];
 	PREGULARRATES pstuRegularates5 = &g_szBankRegularRates[1].stuRegularRates[5];
 	PREGULARRATES pstuRegularates6 = &g_szBankRegularRates[1].stuRegularRates[6];
-	STORAGETIME stuInStorageTime = {0, 18};
+	STORAGETIME stuInStorageTime = {0, 1};
 	RG::CRateCalc clsRateCalc;
 	double dPrincipalInterest = clsRateCalc.GetPrincipalInterest(10000, pstuRegularates, stuInStorageTime);
 	double dPrincipalInterest2 = clsRateCalc.GetPrincipalInterest(10000, pstuRegularates2, stuInStorageTime);
@@ -35,13 +36,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	double dPrincipalInterest5 = clsRateCalc.GetPrincipalInterest(10000, pstuRegularates5, stuInStorageTime);
 	double dPrincipalInterest6 = clsRateCalc.GetPrincipalInterest(10000, pstuRegularates6, stuInStorageTime);
 
-	STORAGETIME stuInIntervalTime = {0, 1};
-	PREGULARRATES pstuRegularates7 = &g_szBankRegularRates[1].stuRegularRates[6];
-	double dPrincipalInterest7 = clsRateCalc.GetRegular(10000, pstuRegularates7, stuInIntervalTime, stuInStorageTime);
+	STORAGETIME stuInIntervalTime = {1, 1};
+	PREGULARRATES pstuRegularates7 = &g_szBankRegularRates[5].stuRegularRates[6];
+	double dPrincipalInterest7 = clsRateCalc.GetRegular(1000, pstuRegularates7, stuInIntervalTime, stuInStorageTime);
 
 
 
 
 	return 0;
 }
-
