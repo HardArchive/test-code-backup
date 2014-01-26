c70b510c107a16c7ebbf9b9b2d6ebf2b527758b1
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2014-01-26 06:44:42 +0000

    MD5加密 32位[大] 32位[小]

---
+++ trunk/WTL/UseMD5ofWTL/UseMD5ofWTL.clw
@@ -0,0 +1,57 @@
+; CLW file contains information for the MFC ClassWizard
+
+[General Info]
+Version=1
+LastClass=
+LastTemplate=CDialog
+NewFileInclude1=#include "stdafx.h"
+NewFileInclude2=#include "usemd5ofwtl.h"
+LastPage=0
+
+ClassCount=0
+
+ResourceCount=3
+Resource1=IDD_ABOUTBOX
+Resource2=IDD_MAINDLG
+Resource3=IDR_MAINFRAME
+
+[DLG:IDD_ABOUTBOX]
+Type=1
+Class=?
+ControlCount=5
+Control1=IDOK,button,1342242817
+Control2=IDC_STATIC,static,1342308353
+Control3=IDC_STATIC,static,1342177283
+Control4=IDC_STATIC,button,1342177287
+Control5=IDC_STATIC,static,1342308352
+
+[DLG:IDD_MAINDLG]
+Type=1
+Class=?
+ControlCount=6
+Control1=IDOK,button,1342242817
+Control2=IDCANCEL,button,1342242816
+Control3=ID_APP_ABOUT,button,1342242816
+Control4=IDC_EDIT1,edit,1350631552
+Control5=IDC_STATIC_RESUAL,static,1350696960
+Control6=IDC_STATIC,static,1342308352
+
+[ACL:IDR_MAINFRAME]
+Type=1
+Class=?
+Command1=ID_FILE_NEW
+Command2=ID_FILE_OPEN
+Command3=ID_FILE_SAVE
+Command4=ID_FILE_PRINT
+Command5=ID_EDIT_UNDO
+Command6=ID_EDIT_CUT
+Command7=ID_EDIT_COPY
+Command8=ID_EDIT_PASTE
+Command9=ID_EDIT_UNDO
+Command10=ID_EDIT_CUT
+Command11=ID_EDIT_COPY
+Command12=ID_EDIT_PASTE
+Command13=ID_NEXT_PANE
+Command14=ID_PREV_PANE
+CommandCount=14
+
---
+++ trunk/WTL/UseMD5ofWTL/maindlg.cpp
@@ -0,0 +1,69 @@
+// maindlg.cpp : implementation of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "resource.h"
+
+#include "aboutdlg.h"
+#include "maindlg.h"
+
+BOOL CMainDlg::PreTranslateMessage(MSG* pMsg)
+{
+	return IsDialogMessage(pMsg);
+}
+
+BOOL CMainDlg::OnIdle()
+{
+	return FALSE;
+}
+
+LRESULT CMainDlg::OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+	// center the dialog on the screen
+	CenterWindow();
+
+	// set icons
+	HICON hIcon = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+		IMAGE_ICON, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
+	SetIcon(hIcon, TRUE);
+	HICON hIconSmall = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+		IMAGE_ICON, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
+	SetIcon(hIconSmall, FALSE);
+
+	// register object for message filtering and idle updates
+	CMessageLoop* pLoop = _Module.GetMessageLoop();
+	ATLASSERT(pLoop != NULL);
+	pLoop->AddMessageFilter(this);
+	pLoop->AddIdleHandler(this);
+
+	UIAddChildWindowContainer(m_hWnd);
+
+	return TRUE;
+}
+
+LRESULT CMainDlg::OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	CAboutDlg dlg;
+	dlg.DoModal();
+	return 0;
+}
+
+LRESULT CMainDlg::OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	// TODO: Add validation code 
+	CloseDialog(wID);
+	return 0;
+}
+
+LRESULT CMainDlg::OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+{
+	CloseDialog(wID);
+	return 0;
+}
+
+void CMainDlg::CloseDialog(int nVal)
+{
+	DestroyWindow();
+	::PostQuitMessage(nVal);
+}
