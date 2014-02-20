4ff9d191b9153100cad15fc2c1960b8cdb60a41c
happyhaoyun@gmail.com <happyhaoyun@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2014-02-20 08:00:12 +0000

    热键注册演示程序

---
+++ WTL/HotKey/HotKey/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HotKey.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_MAINDLG                     129
+#define IDC_BUTTON_REGEST_WINDOW_HOTKEY 1001
+#define IDC_BUTTON_REGEST_SYSTEM_HOTKEY 1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON_UNREGEST_HOTKEY      1004
+#define IDC_HOTKEY_WINDOW               1005
+#define IDC_HOTKEY_SYSTEM               1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
