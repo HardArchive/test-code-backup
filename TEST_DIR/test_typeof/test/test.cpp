b4fad58ff0fd61f6a5e856f2fd4e2a80418963cf
wubuxiansheng@gmail.com <wubuxiansheng@gmail.com@5bc701ee-a69d-d4ec-bd00-c7d7e4f2e1cb>
2014-09-05 10:03:36 +0000

    前++与后++的奇怪性验证  未解决未理解

--- TEST_DIR/test_typeof/test/test.cpp
+++ TEST_DIR/test_typeof/test/test.cpp
@@ -15,8 +15,15 @@ typedef struct TEST_ND
 	char b;
 	wchar_t c;
 	long d;
-}TestND, *pTestND;
+}TestND, *PTestND;
 
+#define get(a) ({item = a; 1;})
+
+int Getabc()
+{
+	int i = 5;
+	return 5;
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int a = 4;
@@ -30,12 +37,37 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	TestND stuTestND = {0};
 
-	pTestND pstuTemp = NULL;
+	PTestND pstuTemp = NULL;
 
-	if(typeid(pstuTemp) == typeid(pTestND))
+	if(typeid(*pstuTemp) == typeid(TestND))
 	{
 		printf("%s\n", "type is same");
 	}
+
+	printf("  123");  printf("   321\r\n");
+
+	int item = 0;
+
+	//bool bFlag = get(3);
+
+	int i= 0, j = 3;
+	printf("%d,%d, %d\r\n", i++, i++, i);   //1,0,2
+
+	i= 0;
+	printf("%d,%d\r\n", ++i, ++i);           //2,2
+
+
+	i = 0;
+	printf("%d,%d,%d,%d", Getabc(), i, i++, i);
+
+	for(int j=i; (j>2)&&(j<50);j++)
+	{
+		printf("%d\r\n", j);
+	}
+
+
+
+	//typeid(*pstuTestND) temp ;
 	return 0;
 }
 
